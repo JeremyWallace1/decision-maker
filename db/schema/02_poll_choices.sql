d0c5f86ce9a6dcb886c15d969313af855fec85af
Mark Bieganek <mark.bieganek@telus.com>
2022-12-17 12:25:05 -0700

    Rename 05_widgets.sql > 02_poll_choices.sql. Modify schema

---
+++ db/schema/02_poll_choices.sql
@@ -0,0 +1,9 @@
+-- Drop and recreate poll_choices table
+
+DROP TABLE IF EXISTS poll_choices CASCADE;
+CREATE TABLE poll_choices (
+  id SERIAL PRIMARY KEY NOT NULL,
+  poll_id INTEGER REFERENCES polls(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT
+);
