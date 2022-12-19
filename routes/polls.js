41f660a617710455337d72ba447637db096a17a2
Mark Bieganek <mark.bieganek@telus.com>
2022-12-19 11:04:07 -0700

    Create variables for /polls POST data, log to console

--- routes/polls.js
+++ routes/polls.js
@@ -10,7 +10,23 @@ const express = require('express');
 const router  = express.Router();
 
 router.post('/', (req, res) => {
-  console.log(req.body);
-}); 
+  const { email, title, question } = req.body;
+  const answerTitle1 = req.body.answer1_title;
+  const answerTitle2 = req.body.answer2_title;
+  const answerTitle3 = req.body.answer3_title;
+  const answerDescription1 = req.body.answer1_description;
+  const answerDescription2 = req.body.answer2_description;
+  const answerDescription3 = req.body.answer3_description;
+  console.log('email: ', email);
+  console.log('title: ', title);
+  console.log('question: ', question);
+  console.log('answerTitle1: ', answerTitle1);
+  console.log('answerTitle2: ', answerTitle2);
+  console.log('answerTitle3: ', answerTitle3);
+  console.log('answerDescription1: ', answerDescription1);
+  console.log('answerDescription2: ', answerDescription2);
+  console.log('answerDescription3: ', answerDescription3);
+  }
+); 
 
 module.exports = router;
