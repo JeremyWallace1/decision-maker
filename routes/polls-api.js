5d40d8bf0a9976ef423e263ad51219637457e704
Mark Bieganek <mark.bieganek@telus.com>
2022-12-19 21:20:58 -0700

    Update route GET /apis/polls/:id to return poll config data

--- routes/polls-api.js
+++ routes/polls-api.js
@@ -12,7 +12,25 @@ const pollQueries = require('../db/queries/polls');
 
 // creator views poll results & links for sharing
 router.get('/:id', (req, res) => {
-  res.status(200).send('GET route /polls/:id ***Coming soon...***');
+  const pollId = req.params.id;
+  const data = {};
+  pollQueries.getPoll(pollId)
+    .then(poll => {
+      data.config = {
+        id: poll.id,
+        creator_email: poll.creator_email,
+        question: poll.question,
+        description: poll.description,
+        results_url: poll.results_url,
+        sharing_url: poll.sharing_url,
+      };
+      return poll;
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
 });
 
 module.exports = router;
