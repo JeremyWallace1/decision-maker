7e8fb5ef8bbb93ece7d22cfe6b6501dce7c86569
Mark Bieganek <mark.bieganek@telus.com>
2022-12-19 17:41:45 -0700

    Include express-validator, validate email and poll question

--- routes/polls.js
+++ routes/polls.js
@@ -8,16 +8,33 @@
 const { response } = require('express');
 const express = require('express');
 const router  = express.Router();
+const { check, validationResult } = require('express-validator');
 const pollQueries = require('../db/queries/polls');
 
-router.post('/', (req, res) => {
-  // TODO: validation of data
-  // TODO: better randomization, not just numerical
-  req.body.results_url = 'r' + Math.floor(Math.random() * 99999) + 1;
-  req.body.sharing_url = 's' + Math.floor(Math.random() * 99999) + 1;
+router.post('/',
+  // Use express-validator middleware to validate the post request
+  [
+    check('email')
+      .isEmail()
+      .withMessage("please enter a valid email"),
+    check('question')
+      .isLength({ min: 5})
+      .withMessage("the poll question isn't descriptive long enough, please lengthen it")
+      .isLength({ max: 255})
+      .withMessage("the poll question is too long, please shorten it")
+      .trim(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req).formatWith(({ msg }) => msg);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // TODO: better randomization, not just numerical
+    req.body.results_url = 'r' + Math.floor(Math.random() * 99999) + 1;
+    req.body.sharing_url = 's' + Math.floor(Math.random() * 99999) + 1;
 
-  pollQueries.createPoll(req.body)
-  .then(data => {
+    pollQueries.createPoll(req.body)
+    .then(data => {
         const choicePromises = [];
         let countChoice = 0;
         while (true) {
