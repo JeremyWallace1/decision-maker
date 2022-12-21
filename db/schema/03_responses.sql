346b1cf18ea6b69eb4e23cde1449bc5aa362a77f
Mark Bieganek <mark.bieganek@telus.com>
2022-12-20 17:04:57 -0700

    Modify 03_responses.sql schema to include rank_score column

--- db/schema/03_responses.sql
+++ db/schema/03_responses.sql
@@ -5,5 +5,6 @@ CREATE TABLE responses (
   id SERIAL PRIMARY KEY NOT NULL,
   poll_id INTEGER REFERENCES polls(id) ON DELETE CASCADE,
   choice_id INTEGER REFERENCES poll_choices(id) ON DELETE CASCADE,
-  respondent_ip VARCHAR(50) NOT NULL
+  respondent_ip VARCHAR(50) NOT NULL,
+  rank_score INTEGER CONSTRAINT valid_rank_score CHECK(rank_score >= 0) DEFAULT 0
 );
