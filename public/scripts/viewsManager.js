936dcc3e83d0d2c2ebbd24d42f8338fb813edfa9
Mark Bieganek <mark.bieganek@telus.com>
2022-12-21 16:53:51 -0700

    Setup new poll view in viewsManager

--- public/scripts/viewsManager.js
+++ public/scripts/viewsManager.js
@@ -5,18 +5,21 @@ $(() => {
   window.views_manager = {};
 
   window.views_manager.show = function(item) {
-    $newPollForm.detach();
+    $formPollNew.detach();
 
     switch (item) {
-      case 'newPoll':
-        $newPollForm.appendTo($main);
+      case 'pollNew':
+        $formPollNew.appendTo($main);
         break;
+      case 'pollResults':
+        $pollResults.appendTo($main);
+        break
       case 'error': {
         const $error = $(`<p>${arguments[1]}</p>`);
         $error.appendTo('body');
         setTimeout(() => {
           $error.remove();
-          views_manager.show('newPoll');
+          views_manager.show('pollNew');
         }, 2000);
         
         break;
