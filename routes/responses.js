e2e2ae25d61e00f5517d2a849044c0902fd80a33
Mark Bieganek <mark.bieganek@telus.com>
2022-12-21 10:11:59 -0700

    Add cookie-parser dependency so app can read cookie data

--- routes/responses.js
+++ routes/responses.js
@@ -4,13 +4,6 @@
  *   these routes are mounted onto /responses
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
-const { default: axios } = require('axios');
-const request = require('axios');
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Returns: Promise of request for IP address
- */
-const fetchMyIP = () => axios.get('https://api.ipify.org?format=json');
 
 const { response } = require('express');
 const express = require('express');
@@ -18,11 +11,11 @@ const router  = express.Router();
 const { body, validationResult } = require('express-validator');
 const responseQueries = require('../db/queries/polls');
 
+const cookieParser = require('cookie-parser');
+
 // user responds to a poll
 router.post('/:id', (req, res) => {
-  fetchMyIP()
-    .then(data => console.log(data.data.ip));
-    // .finally(console.log(req.body));
+  console.log(req.cookies);
   res.status(200).send('POST route /responses/:id ***Coming soon...***');
 });
 
