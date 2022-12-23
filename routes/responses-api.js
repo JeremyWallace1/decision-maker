5901de843952b090e94dbaa36816ce22547bbb1e
Mark Bieganek <mark.bieganek@telus.com>
2022-12-23 14:22:16 -0700

    Build API GET route /:uri

--- routes/responses-api.js
+++ routes/responses-api.js
@@ -10,22 +10,44 @@ const express = require('express');
 const router  = express.Router();
 const responseQueries = require('../db/queries/polls');
 
-// user views poll and values and can modify selections before posting
-router.get('/:id', (req, res) => {
-  const pollId = Number(req.params.id);
+// API request for responses data with shortcode uri reference
+router.get('/:uri', (req, res) => {
+  const uri = req.params.uri;
   const responsesData = {};
   const outputData = [];
-  responsesData.pollId = pollId;
-  responseQueries.getPollResponses(pollId)
-    .then(pollResponses => responsesData.responses = pollResponses)
-    .then(pollResponses => responseQueries.sumResponseScores(pollId))
+  let pollId = null;
+  console.log('start')
+  responseQueries.getPollByResultsUri(uri)
+    .then(data => responsesData.poll_id = data.id)
+    .then(data => responseQueries.getPollResponses(responsesData.poll_id))
+    .then(data => responsesData.responses = data)
+    .then(data => responseQueries.sumResponseScores(responsesData.poll_id))
     .then(scores => responsesData.scores = scores)
     .then(output => outputData.push(responsesData))
     .then(output => res.json(outputData))
     .catch(err => {
       res.status(500)
-        .json({ error: err.message });
+      .json({ error: err.message });
     });
 });
+
+
+// user views poll and values and can modify selections before posting
+// router.get('/:id', (req, res) => {
+//   const pollId = Number(req.params.id);
+//   const responsesData = {};
+//   const outputData = [];
+//   responsesData.pollId = pollId;
+//   responseQueries.getPollResponses(pollId)
+//     .then(pollResponses => responsesData.responses = pollResponses)
+//     .then(pollResponses => responseQueries.sumResponseScores(pollId))
+//     .then(scores => responsesData.scores = scores)
+//     .then(output => outputData.push(responsesData))
+//     .then(output => res.json(outputData))
+//     .catch(err => {
+//       res.status(500)
+//         .json({ error: err.message });
+//     });
+// });
 
 module.exports = router;
