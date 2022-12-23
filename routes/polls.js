f3e19399d3d0bb8ac2c101770dc3c745c7c280f8
Mark Bieganek <mark.bieganek@telus.com>
2022-12-22 19:27:52 -0700

    Add route for polls/:uri, return JSON poll data

--- routes/polls.js
+++ routes/polls.js
@@ -11,10 +11,38 @@ const router  = express.Router();
 const { body, validationResult } = require('express-validator');
 const pollQueries = require('../db/queries/polls');
 
-// creator landing page to create new poll
-router.get('/', (req, res) => {
-  res.redirect('/');
-  // res.status(200).send('GET route /polls/ ***Coming soon...***');
+// API request for poll data with shortcode uri reference
+router.get('/:uri', (req, res) => {
+  const uri = req.params.uri;
+  const pollData = {};
+  const outputData = [];
+  const promises = [];
+  promises.push(pollQueries.getPollByResultsUri(uri));
+  promises.push(pollQueries.getPollBySharingUri(uri));
+  Promise.all(promises)
+    .then(all => {
+      if (all[0]) {
+        pollData.config = all[0];
+        return pollData.config.id;
+      } else if (all[1]) {
+        pollData.config = all[1];
+        return pollData.config.id;
+      }
+      throw new Error('Poll not found!');
+      // res.status(404).send('404 - Poll not found!');
+    })
+    .then(pollId => pollQueries.getPollChoices(pollId))
+    .then(choices => pollData.choices = choices)
+    .then(output => outputData.push(pollData))
+    .then(output => res.json(outputData))
+    .catch(err => {
+      if (err.message === 'Poll not found!') {
+        return res.status(404)
+          .json({ error: err.message });
+      }
+      return res.status(500)
+        .json({ error: err.message });
+    });
 });
 
 // creator submits new poll
