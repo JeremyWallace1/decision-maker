1c9b81fb5774500e4c381215ad34e59b26b6c33f
Mark Bieganek <mark.bieganek@telus.com>
2022-12-24 18:33:39 -0700

    Add hidden input elements with ids

--- public/scripts/formPollRespond.js
+++ public/scripts/formPollRespond.js
@@ -7,46 +7,44 @@ $(() => {
   const getHtml = (data) => {
     const poll = data;
     let buffer = `
-    <form action="/responses/${poll.config.id}" method="POST" id="poll-response" class="poll">
-      <header class="poll_heading">
-        <div class="row mb-2"></div>
-        <div class="row mb-3">
-          <h5 id="email${poll.config.id}">${poll.config.creator_email} has asked a question...</h5>
-        </div>
+      <form action="/responses/${poll.config.id}" method="POST" id="poll-response" class="poll">
+        <header class="poll_heading">
+          <div class="row mb-2"></div>
+          <div class="row mb-3">
+            <h5 id="email${poll.config.id}">${poll.config.creator_email} has asked a question...</h5>
+          </div>
 
-        <hr>
+          <hr>
 
-        <div class="row">
-          <h4 class="col-md-2" id="labelQuestion${poll.config.id}">
-            Question:
-          </h4>
-          <h4 class="col-md-10" id="question${poll.config.id}">
-            ${poll.config.question}
-          </h4>
-        </div>
+          <div class="row">
+            <h4 class="col-md-2" id="labelQuestion${poll.config.id}">
+              Question:
+            </h4>
+            <h4 class="col-md-10" id="question${poll.config.id}">
+              ${poll.config.question}
+            </h4>
+          </div>
 
-        <div class="row">
-          <p class="col-md-12" id="description${poll.config.id}">
-            ${poll.config.description}
-          </p>
-        </div>
+          <div class="row">
+            <p class="col-md-12" id="description${poll.config.id}">
+              ${poll.config.description}
+            </p>
+          </div>
 
-        <hr class="major">
-        
-      </header>
+          <hr class="major">
+          
+        </header>
       `;
 
-      let count = 0;
-      for (const choice in poll.choices) {
-        count++;
-       buffer += `
+    let count = 0;
+    for (const choice in poll.choices) {
+      count++;
+      buffer += `
         <div class="row">
           <h5 class="col-md-2" id="labelAnswer${poll.choices[choice].id}">Answer #${count}:</h5>
           <h6 class="col-md-10" id="answer${poll.choices[choice].id}">${poll.choices[choice].title}</h6>
-        </div>
-    
-        <div class="row">
           <p class="col-md-12" id="description${poll.choices[choice].id}">${poll.choices[choice].description}</p>
+          <input type="hidden" name="answer" value="${poll.choices[choice].id}" />
         </div>
 
         <hr class="minor">
@@ -56,7 +54,8 @@ $(() => {
     buffer += `
         <footer class="poll_footer row mb-3">
           <div class="buttons">
-            <button type="submit" class="button button-large col-12">Submit Choices</button> 
+            <button type="submit" class="button button-large col-12">Submit Choices</button>
+            <input type="hidden" name="poll_id" value="${poll.config.id}" />
           </div>
         </footer>
       </form>
@@ -76,9 +75,10 @@ $(() => {
   const attachEventListener = ($element) => {
     $element.on('submit', function (event) {
       event.preventDefault();
-      console.log('test')
+      console.log('test');
       views_manager.show('none');
-      
+      const data = $(this).serialize();
+      console.log(data);
     });
   }
 
