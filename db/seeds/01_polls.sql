d2b7063cbd29e6b0a980a1a639a45bd7d87cd474
Mark Bieganek <mark.bieganek@telus.com>
2022-12-28 11:08:58 -0700

    Modify seed data, share url prefixed with s rather than p

--- db/seeds/01_polls.sql
+++ db/seeds/01_polls.sql
@@ -1,3 +1,3 @@
 -- polls table seeds here
-INSERT INTO polls (creator_email, question, description, results_url, sharing_url) VALUES ('test1@test.com', 'What gift should we give to Danny?', 'Birthday is coming next Friday, so let''s have a decision by then!', 'r12345', 'p12345');
-INSERT INTO polls (creator_email, question, description, results_url, sharing_url) VALUES ('test2@test.com', 'Which movie should we go see?', 'If anyone answers Black Adam, we''re no longer friends!', 'r98765', 'p98765');
+INSERT INTO polls (creator_email, question, description, results_url, sharing_url) VALUES ('test1@test.com', 'What gift should we give to Danny?', 'Birthday is coming next Friday, so let''s have a decision by then!', 'r12345', 's12345');
+INSERT INTO polls (creator_email, question, description, results_url, sharing_url) VALUES ('test2@test.com', 'Which movie should we go see?', 'If anyone answers Black Adam, we''re no longer friends!', 'r98765', 's98765');
