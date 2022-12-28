34bee6d4431d121054e788fa1e9a53f83f1e59d9
Mark Bieganek <mark.bieganek@telus.com>
2022-12-28 11:03:21 -0700

    Change port back to 8080

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 // app.set('view engine', 'ejs');
