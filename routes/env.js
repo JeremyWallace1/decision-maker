a31904cce5ff84d96b193844ee63a9938ced106f
Mark Bieganek <mark.bieganek@telus.com>
2022-12-28 17:51:06 -0700

    Abstract IP lookup to getIP function in lib/helpers.js

--- routes/env.js
+++ routes/env.js
@@ -8,21 +8,15 @@
 const { response } = require('express');
 const express = require('express');
 const router  = express.Router();
-const axios = require('axios');
+const helpers = require('../lib/helpers.js');
 
-// user responds to a poll
 router.get('/ip', (req, res) => {
-  switch (process.env.ENV_TYPE) {
-    case ('development') :
-      res.json({ ip: process.env.DEV_IP });
-    case ('staging') :
-      res.json({ ip: process.env.STAGING_IP });
-    case ('production') :
-      axios.get('https://api.ipify.org')
-        .then(data => res.json({ ip: data.data }))
-        .catch(err => console.log('Error retrieving IP: ', err.message))
-  }
- 
+  helpers.getIP()
+    .then(data => res.json(data))
+    .catch(err => {
+      return res.status(500)
+          .json({ error: err.message });
+    });
 });
 
 module.exports = router;
