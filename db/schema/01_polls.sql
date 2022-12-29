7950d7ce1fcf5dd80b07608f8f7ef6659c863113
Mark Bieganek <mark.bieganek@telus.com>
2022-12-29 14:57:58 -0700

    Update polls schema to include image column

--- db/schema/01_polls.sql
+++ db/schema/01_polls.sql
@@ -5,6 +5,7 @@ CREATE TABLE polls (
   id SERIAL PRIMARY KEY NOT NULL,
   creator_email VARCHAR(255) NOT NULL,
   question VARCHAR(255) NOT NULL,
+  image TEXT,
   description TEXT,
   results_url VARCHAR(255) NOT NULL,
   sharing_url VARCHAR(255) NOT NULL
