a43654c67e0adab6b1ef4f78e64b5af00c0a1702
Mark Bieganek <mark.bieganek@telus.com>
2023-01-01 16:43:25 -0700

    Fix: change function name getIp to getIP

--- routes/env.js
+++ routes/env.js
@@ -8,14 +8,14 @@
 const { response } = require('express');
 const express = require('express');
 const router  = express.Router();
-const { getIp } = require('../lib/helpers.js');
+const { getIP } = require('../lib/helpers.js');
 
 router.get('/ip', (req, res) => {
-  getIp()
+  getIP()
     .then(data => res.json(data))
     .catch(err => {
       return res.status(500)
-        .send('500 - Internal Server Error. ' + err.message);
+        .json({ error: err.message });
     });
 });
 
