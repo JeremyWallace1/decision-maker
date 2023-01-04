b7e029f65dac871e5bf8beb0fc030b122f2053e8
Mark Bieganek <mark.bieganek@telus.com>
2023-01-04 12:30:58 -0700

    Remove image when remove image button pressed

--- public/scripts/formPollNew.js
+++ public/scripts/formPollNew.js
@@ -117,8 +117,10 @@ $(() => {
       </div>
     </div>
 
-    <!-- IMAGE INPUT -->
+    <!-- IMAGE -->
     <div class="form-group row mb-3 justify-content-end" id="containerImage:id:">
+
+      <!-- IMAGE INPUT -->
       <label for="choice:id:_image" class="col-md-2 col-form-label">
         Add Image:
       </label>
@@ -132,10 +134,13 @@ $(() => {
           capture="user"
         />
       </div>
+
+      <!-- IMAGE PREVIEW -->
       <div class="col-md-10 mt-3" id="containerPreviewImage:id:">
         <img src="" class="img-fluid img-thumbnail d-block question img-preview" id="imgPreviewImage:id:" />
         <button class="button my-2" id="buttonRemoveImage:id:">remove image</button>
       </div>
+
     </div>
 
     <!-- DESCRIPTION INPUT -->
@@ -150,7 +155,7 @@ $(() => {
 
     <!-- SHOW INPUTS -->
     <div class="row mb-3 justify-content-end" id="containerAddInput:id:">
-      
+
       <!-- SHOW IMAGE INPUT -->
       <div class="col-md-10" id="containerAddInputImage:id:">
         <button type="button" class="button-link" id="buttonAddInputImage:id:"><i class="fa-regular fa-image fa-lg">&nbsp</i>add image</button>
@@ -219,28 +224,41 @@ $(() => {
     const countChoice = totalChoices + 1;
     const parsedHTML = choiceHTML.replaceAll(':id:', countChoice);
     const $choice = $(parsedHTML);
+    $formPollNew.images[`image${countChoice}`] = null;
     
     // Hide elements that should not be shown initially
-    $choice.find(`#containerDescription${countChoice}`)
-      .hide(0);
-    $choice.find(`#containerImage${countChoice}`)
-      .hide(0);
-    $choice.find(`#containerPreviewImage${countChoice}`)
-      .hide(0);
+    $choice.find(`#containerDescription${countChoice}`).hide();
+    $choice.find(`#containerImage${countChoice}`).hide();
+    $choice.find(`#containerPreviewImage${countChoice}`).hide();
 
-    // Add event listeners to elements that require them
+    // Add event listeners
+    // Button press: Add image
     $choice.find(`#buttonAddInputImage${countChoice}`)
-      .on('click', function() {
+      .on('click', function(event) {
+        event.preventDefault();
         $choice.find(`#containerImage${countChoice}`).show(300);
         $choice.find(`#containerAddInputImage${countChoice}`).hide(300);
       }
     )
+    // Button press: Add description
     $choice.find(`#buttonAddInputDescription${countChoice}`)
-      .on('click', function() {
+      .on('click', function(event) {
+        event.preventDefault();
         $choice.find(`#containerDescription${countChoice}`).show(300);
         $choice.find(`#containerAddInputDescription${countChoice}`).hide(300);
       }
     )
+    // Button press: Remove image
+    $choice.find(`#buttonRemoveImage${countChoice}`)
+      .on('click', function(event) {
+        event.preventDefault();
+        $formPollNew.images[`image${countChoice}`] = null;
+        $choice.find(`#containerPreviewImage${countChoice}`).hide(300);
+        $choice.find(`#imgPreviewImage${countChoice}`).attr('src', '');
+        $choice.find(`#inputImage${countChoice}`).val('');
+      }
+    )
+    // Input change: Choose image file
     $choice.find(`#inputImage${countChoice}`)
       .on('change', function (event) {
         const file = this.files[0];
