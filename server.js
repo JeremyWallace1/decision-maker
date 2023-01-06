025c0cf59e549bb5e1bf9a636170f14221330fff
Mark Bieganek <mark.bieganek@telus.com>
2023-01-06 14:20:03 -0700

    Lint pass: server.js

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ const app = express();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
-app.use(express.urlencoded({ 
-    extended: true,
-    limit: '60mb',
-  })
-);
+app.use(express.urlencoded({
+  extended: true,
+  limit: '60mb',
+}));
 app.use(
   '/styles',
   sassMiddleware({
