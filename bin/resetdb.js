2d4ee1c61e27bb7ab4e931ff6139070b5176ff43
Mark Bieganek <mark.bieganek@telus.com>
2023-01-06 13:23:51 -0700

    Remove deprecated comments

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -6,11 +6,6 @@ const fs = require('fs');
 const chalk = require('chalk');
 const db = require('../db/connection');
 
-// PG connection setup
-// const connectionString = process.env.DATABASE_URL ||
-//   `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
-// const client = new Client();
-
 // Loads the schema files from db/schema
 const runSchemaFiles = async () => {
   console.log(chalk.cyan(`-> Loading Schema Files ...`));
